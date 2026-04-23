d766ec920f65e5608f339c59e4f8b5924053074b
PROSPEKT <prospektweb@gmail.com>
2026-04-23 19:48:50 +0500

    Create install/version.php with module version information

---
+++ install/version.php
@@ -0,0 +1,10 @@
+<?php
+
+// Module version information
+$module_version = [
+    'version' => '1.0.0',
+    'release_date' => '2026-04-23 14:48:31',
+];
+
+// Return the module version
+return $module_version;
