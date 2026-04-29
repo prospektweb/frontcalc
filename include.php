2b1d7dd4eb7f4ba2ed0e85af7d44d4041d489e86
PROSPEKT <prospektweb@gmail.com>
2026-04-29 14:19:17 +0500

    Revert "Resolve module path, update autoload registration, and move admin event handler registration into install/uninstall"

--- include.php
+++ include.php
@@ -2,26 +2,7 @@
 
 use Bitrix\Main\Loader;
 
-$moduleId = 'prospektweb.frontcalc';
-$moduleBasePath = null;
-$docRoot = isset($_SERVER['DOCUMENT_ROOT']) ? rtrim((string)$_SERVER['DOCUMENT_ROOT'], '/\\') : '';
-
-if ($docRoot !== '') {
-    $localModulePath = $docRoot . '/local/modules/' . $moduleId;
-    $bitrixModulePath = $docRoot . '/bitrix/modules/' . $moduleId;
-
-    if (is_dir($localModulePath)) {
-        $moduleBasePath = $localModulePath;
-    } elseif (is_dir($bitrixModulePath)) {
-        $moduleBasePath = $bitrixModulePath;
-    }
-}
-
-if ($moduleBasePath === null) {
-    $moduleBasePath = __DIR__;
-}
-
-Loader::registerAutoLoadClasses($moduleId, [
-    'Prospektweb\\Frontcalc\\Admin\\ProductCardButton' => $moduleBasePath . '/lib/Admin/ProductCardButton.php',
-    'Prospektweb\\Frontcalc\\Service\\CalculatorAvailability' => $moduleBasePath . '/lib/Service/CalculatorAvailability.php',
+Loader::registerAutoloadClasses('prospektweb.frontcalc', [
+    'Prospektweb\\Frontcalc\\Admin\\ProductCardButton' => 'lib/Admin/ProductCardButton.php',
+    'Prospektweb\\Frontcalc\\Service\\CalculatorAvailability' => 'lib/Service/CalculatorAvailability.php',
 ]);
