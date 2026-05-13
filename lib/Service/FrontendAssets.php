4fa87d4ed1014c1285c53cc21160c1fa25ae56de
PROSPEKT <prospektweb@gmail.com>
2026-05-13 15:56:35 +0500

    Add frontcalc auth intent handler

--- lib/Service/FrontendAssets.php
+++ lib/Service/FrontendAssets.php
@@ -15,10 +15,17 @@ class FrontendAssets
         $config = new ModuleConfig();
         $hiddenIds = $config->getHiddenOfferValueIds();
 
+        global $USER;
+
+        $isAuthorized = is_object($USER) && method_exists($USER, 'IsAuthorized') && $USER->IsAuthorized();
+
         $asset = Asset::getInstance();
+        $asset->addString('<script>window.FrontcalcAuthConfig=' .
+            \CUtil::PhpToJSObject(['isAuthorized' => $isAuthorized], false, true, true) . ';</script>', true);
         $asset->addCss('/local/modules/prospektweb.frontcalc/assets/css/hide-technical-values.css');
         $asset->addCss('/local/modules/prospektweb.frontcalc/assets/css/prices-popup-ext.css');
         $asset->addJs('/local/modules/prospektweb.frontcalc/assets/js/hide-technical-values.js');
+        $asset->addJs('/local/modules/prospektweb.frontcalc/assets/js/frontcalc-auth.js');
         $asset->addString('<script>window.myModuleHiddenValueIds=' . 
             \CUtil::PhpToJSObject(array_values($hiddenIds), false, true, true) . ';</script>', true);
     }
