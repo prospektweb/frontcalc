1190f2a6b6a25c3a246e0ec580a7c132319b53f4
PROSPEKT <prospektweb@gmail.com>
2026-05-15 12:59:33 +0500

    Update prices_with_edit.php

--- prices_with_edit.php
+++ prices_with_edit.php
@@ -1386,9 +1386,9 @@ class Prices
                             </svg>
                         </button>
                         <style>
-                            .frontcalc_but__openpopup{width:64px;background:none;border:none;fill:#555558;text-align:left;display:flex;align-items:center;padding:0;}
+                            .frontcalc_but__openpopup{width:<?= $this->isDetailPage() ? '64px' : '24px'; ?>;;background:none;border:none;fill:#555558;text-align:left;display:flex;align-items:center;padding:0;}
                             .frontcalc_but__openpopup:hover{fill:var(--theme-base-color);}
-                            .prices--with-popup-table{gap:20px;display:flex;flex-direction:row;align-items:center;}
+                            .prices--with-popup-table{<?= $this->isDetailPage() ? '20px' : '10px'; ?>;display:flex;flex-direction:row;align-items:center;}
                             .price__row {gap: 0px 8px;}
                             .mt {margin-top: 0px;}
                         </style>
